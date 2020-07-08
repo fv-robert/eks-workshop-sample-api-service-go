4b838904899ef0e8e8113294ac7f1be19d02252d
fv-robert <52677231+fv-robert@users.noreply.github.com>
2020-07-08 15:31:24 -0600

    Update main.go
    Example update.

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "I did it!!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
